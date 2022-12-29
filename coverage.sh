cef17d95bc83980ff0950c32a68666639dca47cf
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-12-29 22:46:44 +0100

    Fix coverage script

--- coverage.sh
+++ coverage.sh
@@ -3,4 +3,4 @@
 cargo install grcov
 
 RUSTFLAGS="-C instrument-coverage" cargo test
-find . -iname "*.profraw" | xargs grcov -o coverage_report -t html -s . -b .
+find . -iname "*.profraw" | xargs grcov -o coverage_report -t html -s src -b .
