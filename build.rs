bdc0fe9d6b4d98d5cca242d6d81c60cbe992bd4b
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-12-30 01:18:07 +0100

    tests: ensure that the built 6502 programs are relocated

--- build.rs
+++ build.rs
@@ -13,11 +13,13 @@ fn build_6502_program(folder: &std::path::Path) -> std::path::PathBuf {
     let sh = Shell::new().unwrap();
     sh.create_dir(&test_programs_dir).unwrap();
     cmd!(sh, "{mos_6502_dir}/bin/llvm-mc --arch=mos {folder}/main.s --assemble --filetype=obj -o {test_programs_dir}/{program_name}.o").run().unwrap();
-    cmd!(sh, "{mos_6502_dir}/bin/llvm-objcopy -O binary {out_dir}/test_programs/{program_name}.o {test_programs_dir}/{program_name}").run().unwrap();
+    cmd!(sh, "{mos_6502_dir}/bin/ld.lld -T tests/test.ld -o {test_programs_dir}/{program_name} {test_programs_dir}/{program_name}.o").run().unwrap();
+    cmd!(sh, "{mos_6502_dir}/bin/llvm-objcopy -O binary {out_dir}/test_programs/{program_name} {test_programs_dir}/{program_name}.bin").run().unwrap();
 
     std::path::Path::new(&out_dir)
         .join("test_programs")
         .join(program_name)
+        .with_extension("bin")
         .canonicalize()
         .unwrap()
 }
@@ -32,6 +34,7 @@ fn main() {
         .map(|entry| entry.unwrap().path());
 
     println!("cargo:rerun-if-changed=tests/data");
+    println!("cargo:rerun-if-changed=tests/test.ld");
 
     for folder in folders {
         let program_file = build_6502_program(&folder);
